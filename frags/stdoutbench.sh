7348eea5d18a9d4b228f3afa34126fce8fbdd732
SDRausty <27742457+SDRausty@users.noreply.github.com>
2021-06-28 07:39:07 -0400

    commit 250606490 on 20210628

--- frags/stdoutbench.sh
+++ frags/stdoutbench.sh
@@ -1,8 +1,9 @@
-#!/bin/bash -e
-# Copyright 2017-2018 by SDRausty. All rights reserved.  🌎 🌍 🌏 🌐 🗺
+#!/usr/bin/env bash
+# Copyright 2017-2021 by SDRausty. All rights reserved.  🌎 🌍 🌏 🌐 🗺
 # Hosting https://sdrausty.github.io/TermuxArch courtesy https://pages.github.com
 # https://sdrausty.github.io/TermuxArch/CONTRIBUTORS Thank you for your help.  
 # https://sdrausty.github.io/TermuxArch/README has information about this project. 
 ################################################################################
+set -e
 
 dd if=/dev/urandom bs=1K count=100 | hexdump -C >500KBfile.txt ; for i in {1..4}; do echo $i ; sleep 1 ; time nice -n 20 cat 500KBfile.txt ; sleep 2 ; done
