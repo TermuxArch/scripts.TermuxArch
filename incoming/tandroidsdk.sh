b43f860b628f1019dd20f5b310240225c00ae2ff
SDRausty <27742457+SDRausty@users.noreply.github.com>
2021-07-16 02:18:40 -0400

    commit 349492226 on 20210716

--- incoming/tandroidsdk.sh
+++ incoming/tandroidsdk.sh
@@ -1,7 +1,18 @@
+#!/usr/bin/env sh
 [ -d sdk ] && cd "$HOME/sdk" || mkdir -p "$HOME/sdk" && cd "$HOME/sdk"
-[ -f index.html ] || curl -OL https://developer.android.com/studio/index.html
-NDKTOOLSFILE="$(grep commandlinetools-linux index.html | grep https)"
+PAGES="ndk sdk studio"
+for ONEPAGE in $PAGES 
+do
+if [ -f "$ONEPAGE" ]
+then
+printf "Already got page %s from https://developer.android.com/%s:  Continuing...\\n" "$ONEPAGE" "$ONEPAGE"
+else
+printf "Getting page %s from https://developer.android.com/%s...\\n" "$ONEPAGE" "$ONEPAGE"
+curl -OL "https://developer.android.com/$ONEPAGE"
+fi
+done
+NDKTOOLSFILE="$(grep commandlinetools-linux studio | grep https)"
 NDKTOOLSFILE="${NDKTOOLSFILE#*\"}"
 NDKTOOLSFILE="${NDKTOOLSFILE%*\"}"
 [ -f "${NDKTOOLSFILE##*\/}" ] || curl -OL "$NDKTOOLSFILE"
-[ -f "${NDKTOOLSFILE##*\/}" ] && unzip "${NDKTOOLSFILE##*\/}"
+[ -f "$HOME/sdk/cmdline-tools/source.properties" ] && grep "Android" "$HOME/sdk/cmdline-tools/source.properties" && printf "%s\\n" "File ${NDKTOOLSFILE##*\/} is downloaded and unzipped." || unzip "${NDKTOOLSFILE##*\/}"
