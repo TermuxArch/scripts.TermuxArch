e740f8cbf4a6060a712088328d3bbd1fc7561cd5
SDRausty <27742457+SDRausty@users.noreply.github.com>
2021-10-02 05:47:16 -0400

    commit 104338267 on 20211002

--- incoming/fperms.sh
+++ incoming/fperms.sh
@@ -3,13 +3,16 @@
 ## hosting termuxarch.github.io/TermuxArch courtesy pages.github.com
 ################################################################################
 set -eu
-printf "%s\n" "Script '${0##*/}': STARTED..."
-PERRS="$(du ~/"$1"/ 2>&1 >/dev/null | sed "s/du: cannot read directory '//g" | sed "s/': Permission denied//g")"
-[ -z "$PERRS" ] || { printf "%s" "Fixing  permissions in '~/$1/': " && for PERR in $PERRS ; do chmod 755 "$PERR" ; done && printf "%s\n" "DONE" ; }
+_SHFUNC_ () {
+PERRS="$(du "$1" 2>&1 >/dev/null | sed "s/du: cannot read directory '//g" | sed "s/': Permission denied//g")"
+[ -z "$PERRS" ] || { printf "%s" "Fixing  permissions in '$1': " && for PERR in $PERRS ; do chmod 755 "$PERR" ; done && printf "%s\n" "DONE" ; }
 SDIRS="apex data"
 for SDIR in $SDIRS
 do
-[ -d ~/"$1/$SDIR/" ] && printf "%s" "Deleting ~/$1/$SDIR/: " && rm -rf ~/"$1/$SDIR/" && printf "%s\n" "DONE"
+[ -d "$1/$SDIR" ] && printf "%s" "Deleting $1/$SDIR: " && rm -rf "$1/$SDIR" && printf "%s\n" "DONE"
 done
 printf "%s\n" "Script '${0##*/}': DONE"
+}
+printf "%s\n" "Script '${0##*/}': STARTED..."
+[ -d "$1" ] && _SHFUNC_ "$@" || printf "%s\n" "Directory '$1' does not exist; Exiting... $(exit)"
 # fperms.sh EOF
